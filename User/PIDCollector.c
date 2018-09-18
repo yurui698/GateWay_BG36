8eb1f8fdd2b1013b6d50bb01bce56d9e357f2426
yurui698 <785994764@qq.com>
2018-09-18 13:53:52 +0800

    2018091802

--- User/PIDCollector.c
+++ User/PIDCollector.c
@@ -9,7 +9,7 @@ void pidcollector_data(void) //传感器采集数据
 		case 0:
 		 pid_param_num=0;//建立每个通道上报参数的顺序，如果该通道不检测参数，后面通道检测的参数自动往前移
 		 
-     if(factory_gateway_set[12]==7)//频率输入;
+     if(factory_gateway_set[12]==27)//水肥流量计频率输入;
 		 {
 			 u8 i;
 			 pidcollector_temp=0;
@@ -24,7 +24,7 @@ void pidcollector_data(void) //传感器采集数据
 			
 		case 1:
 			 
-		 if(factory_gateway_set[15]==7)//频率输入
+		 if(factory_gateway_set[15]==27)//水肥流量计频率输入
 		 {
 			 u8 i;			 
 			 pidcollector_temp=0;
@@ -41,7 +41,7 @@ void pidcollector_data(void) //传感器采集数据
 		break;
 			
 		case 2:
-     	if(factory_gateway_set[24]==7)//频率输入
+     	if(factory_gateway_set[24]==27)//水肥流量计频率输入
 		  {
 			 u8 i;
 			 pidcollector_temp=0;
@@ -57,7 +57,15 @@ void pidcollector_data(void) //传感器采集数据
 	  memcpy(pidCollectors,pidcollector_data_buff,16);	
 
 
-		bytelen3=WriteSingleRegister(37,0,pidCollectors,ReportData3);//每个点2个字节
+		bytelen3=WriteSingleRegister(66,0x0000,pidCollectors,ReportData3);//每个点2个字节
+		memcpy(USART3SendTCB,ReportData3,bytelen3);
+		WriteDataToDMA_BufferTX3(bytelen3);
+	
+		bytelen3=WriteSingleRegister(66,0x0001,pidCollectors+2,ReportData3);//每个点2个字节
+		memcpy(USART3SendTCB,ReportData3,bytelen3);
+		WriteDataToDMA_BufferTX3(bytelen3);
+	
+		bytelen3=WriteSingleRegister(66,0x0002,pidCollectors+4,ReportData3);//每个点2个字节
 		memcpy(USART3SendTCB,ReportData3,bytelen3);
 		WriteDataToDMA_BufferTX3(bytelen3);
 
