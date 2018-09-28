8a56469dd4eb87f9c717b4e2edb1013461a9abc9
yurui698 <785994764@qq.com>
2018-09-28 11:18:52 +0800

    2018092801 修改采集策略60s脉冲数

--- User/PIDCollector.c
+++ User/PIDCollector.c
@@ -13,7 +13,9 @@ void pidcollector_data(void) //传感器采集数据
 		 {
 			 u8 i;
 			 pidcollector_temp=0;
-			 pidcollector_temp=TIM2_FrequencyPC0[freq_I];
+//			 pidcollector_temp=TIM2_FrequencyPC0[freq_I];
+			 for(i=freq_I+1;i<=60;i++){pidcollector_temp=pidcollector_temp+TIM2_FrequencyPC0[i];}
+			 for(i=0;i<freq_I;i++){pidcollector_temp=pidcollector_temp+TIM2_FrequencyPC0[i];}
 			
 			 pidcollector_data_buff[pid_param_num++]      =  pidcollector_temp & 0x00ff;           		//输入频率字节低
 			pidcollector_data_buff[pid_param_num++]      =  (pidcollector_temp & 0xff00)>>8;      		//输入频率字节高；每分钟脉冲数，除以60为频率
@@ -28,13 +30,12 @@ void pidcollector_data(void) //传感器采集数据
 		 {
 			 u8 i;			 
 			 pidcollector_temp=0;
-			 pidcollector_temp=TIM2_FrequencyPA1[freq_I];
-			
+//			 pidcollector_temp=TIM2_FrequencyPA1[freq_I];
+			for(i=freq_I+1;i<=60;i++){pidcollector_temp=pidcollector_temp+TIM2_FrequencyPA1[i];}
+			 for(i=0;i<freq_I;i++){pidcollector_temp=pidcollector_temp+TIM2_FrequencyPA1[i];}
 			 
 			 pidcollector_data_buff[pid_param_num++]      =  pidcollector_temp & 0x00ff;        //输入频率字节低
 			 pidcollector_data_buff[pid_param_num++]      =  (pidcollector_temp & 0xff00)>>8;   //输入频率字节高；每分钟脉冲数，除以60为频率
-//       TD_param_num++;
-//			 TD_param_num++;
 			break;
 			}
 		 
@@ -45,7 +46,9 @@ void pidcollector_data(void) //传感器采集数据
 		  {
 			 u8 i;
 			 pidcollector_temp=0;
-			 pidcollector_temp=TIM2_FrequencyPA0[freq_I];
+//			 pidcollector_temp=TIM2_FrequencyPA0[freq_I];
+			 for(i=freq_I+1;i<=60;i++){pidcollector_temp=pidcollector_temp+TIM2_FrequencyPA0[i];}
+			 for(i=0;i<freq_I;i++){pidcollector_temp=pidcollector_temp+TIM2_FrequencyPA0[i];}
 			 
 			 pidcollector_data_buff[pid_param_num++]      =  pidcollector_temp & 0x00ff;           //输入频率字节低
 			 pidcollector_data_buff[pid_param_num++]      =  pidcollector_temp & 0xff00>>8;  //输入频率字节高；每分钟脉冲数，除以60为频率
